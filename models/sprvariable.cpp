f3499a9086d8ec4b0bf8544c390316afff755ee6
longway34 <longway34@gmail.com>
2017-12-25 20:53:15 +0700

    Start fixed

--- models/sprvariable.cpp
+++ models/sprvariable.cpp
@@ -1,20 +1,2 @@
 #include "sprvariable.h"
 
-//template <typename T>
-//T SepVariable<T>::getData() const
-//{
-//    return data;
-//}
-
-//template <typename T>
-//void SepVariable<T>::setData(const T &value)
-//{
-//    data = value;
-//    setData();
-//}
-
-
-
-//void SPRVariable::fromXML()
-//{
-//}
